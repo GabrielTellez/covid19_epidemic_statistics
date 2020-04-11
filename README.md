7da648a78301f5e775455e9fd0e3532fb0e4db87
Gabriel Tellez <gtellez@uniandes.edu.co>
2020-04-11 00:30:09 -0500

    Initial code to visualize data for different countries at the same starting day with quarantine start date.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # covid19_epidemic_statistics
 Visualization and analysis of COVID-19 epidemic data statistics
+
+## covid19_plots.ipynb
+Jupyter notebook to visualize epidemic data. Compare different countries evolutions from the first day with same number of cases.
+
+## quarantine.py 
+Data for quarantine starting date of several coiuntries.
+
+## tmp
+Scratch area for new code test
+
+## data
+Backups of epidemic statistic data. Up to date data is from 
+https://datahub.io/core/covid-19/datapackage.json
+based from Johns Hopkins University Center for Systems Science and Engineering (CSSE) data.
+
---
+++ quarantine.py
@@ -0,0 +1,37 @@
+# Quarantine start date
+# Data from
+# https://www.businessinsider.com/countries-on-lockdown-coronavirus-italy-2020-3
+
+quarantine = {
+    'United Kingdom': '2020-03-23',
+    'Hungary': '2020-03-28',
+    'Singapore': '2020-04-07',
+    'United Arab Emirates': '2020-03-26',
+    'Russia': '2020-03-20',
+    'South Africa': '2020-03-26',
+    'New Zealand': '2020-03-25',
+    'Saudi Arabia': '2020-03-25',
+    'Colombia': '2020-03-24',
+    'India': '2020-03-24',
+    'Australia': '2020-03-31',
+    'China': '2020-01-23',
+    'Jordan': '2020-03-21',
+    'Argentina': '2020-03-21',
+    'Israel': '2020-03-25',
+    'Belgium': '2020-03-17',
+    'Germany': '2020-03-20',
+    'Malaysia': '2020-03-16',
+    'Czech Republic': '2020-03-16',
+    'France': '2020-03-16',
+    'Morocco': '2020-03-15',
+    'Kenya': '2020-03-15',
+    'Spain': '2020-03-14',
+    'Poland': '2020-03-13',
+    'Kuwait': '2020-03-13',
+    'Ireland': '2020-03-27',
+    'Norway': '2020-03-12',
+    'El Salvador': '2020-03-11',
+    'Denmark': '2020-03-13',
+    'Italy': '2020-03-10'
+}
+
